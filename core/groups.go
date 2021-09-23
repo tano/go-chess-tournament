ef0645d12a545460a5321ffff97c6a68c82ee958
a.tanoshkin <alexander.tanoshkin@gmail.com>
2021-09-23 21:44:29 +0300

    Add more tests.

--- core/groups.go
+++ core/groups.go
@@ -1,5 +1,43 @@
 package core
 
-func Split(participants []string) [][]string {
-	return nil
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Split(participants []string) ([][]string, error) {
+	middle := len(participants) / 2
+	firstHalf := participants[:middle]
+	secondHalf := participants[middle:]
+	shuffle(&firstHalf)
+	shuffle(&secondHalf)
+	if participantsAreOK(participants) == true {
+		firstCopy := make([]string, len(firstHalf))
+		secondCopy := make([]string, len(secondHalf))
+		copy(firstCopy, firstHalf)
+		copy(secondCopy, secondHalf)
+		return [][]string{firstCopy, secondCopy}, nil
+	} else {
+		return nil, errors.New("participants count should be even")
+	}
+
+}
+
+func participantsAreOK(participants []string) bool {
+	if len(participants)%2 == 0 {
+		return true
+	}
+	return false
+}
+
+func shuffle(participants *[]string) {
+	rand.Seed(time.Now().UnixNano())
+	temp := *participants
+	fmt.Printf("data before shuffle: %v\n", *participants)
+	rand.Shuffle(len(temp), func(i, j int) {
+		(*participants)[i], (*participants)[j] = (*participants)[j], (*participants)[i]
+	})
+	fmt.Printf("data after shuffle: %v\n", *participants)
 }
--- core/groups_test.go
+++ core/groups_test.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -18,9 +19,77 @@ func TestSplit(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Split(tt.args.participants); !reflect.DeepEqual(got, tt.wantAmountOfGroups) {
-				t.Errorf("Split() = %v, wantAmountOfGroups %v", got, tt.wantAmountOfGroups)
+			if got, _ := Split(tt.args.participants); !reflect.DeepEqual(len(got), tt.wantAmountOfGroups) {
+				t.Errorf("len of Split() = %v, wantAmountOfGroups %v", got, tt.wantAmountOfGroups)
 			}
 		})
 	}
+}
+
+func TestSplitOddAmountOfParticipants(t *testing.T) {
+	// given
+	participants := []string{"Alex", "Igor", "Olga", "Max", "Vladimir"}
+
+	// when
+	_, error := Split(participants)
+
+	// then
+	if error == nil {
+		t.Errorf("Wanted error but got nil for 5 participants %v", participants)
+	}
+}
+
+func TestRandomOrder(t *testing.T) {
+	// given
+	participants := []string{"Alex", "Igor", "Olga", "Max", "Vladimir", "Vadim"}
+
+	// when
+	firstSplit, _ := Split(participants)
+	secondSplit, _ := Split(participants)
+	thirdSplit, _ := Split(participants)
+
+	// then
+	if len(firstSplit) != len(secondSplit) && len(firstSplit) != len(thirdSplit) {
+		t.Errorf("Size of split for first, second and third split should be equal")
+	}
+
+	if !reflect.DeepEqual(sortAndMerge(firstSplit), sortAndMerge(secondSplit)) || !reflect.DeepEqual(sortAndMerge(secondSplit), sortAndMerge(thirdSplit)) {
+		t.Errorf("Consequence runs produce different results (size)")
+	}
+
+	sameOrder := true
+
+out:
+	for i, firstSplitPart := range firstSplit {
+		for j, elementInFirstSplitPart := range firstSplitPart {
+			if elementInFirstSplitPart != secondSplit[i][j] ||
+				elementInFirstSplitPart != thirdSplit[i][j] ||
+				secondSplit[i][j] != thirdSplit[i][j] {
+				sameOrder = false
+				break out
+			}
+		}
+	}
+
+	if sameOrder == true {
+		t.Errorf("Consequence runs produce same order results")
+	}
+
+}
+
+func sortAndMerge(splitToSort [][]string) []string {
+	result := make([]string, 0)
+	totalSize := 0
+	for i := range splitToSort {
+		totalSize += len(splitToSort[i])
+	}
+	for _, element := range splitToSort {
+		tmp := make([]string, len(element))
+		copy(tmp, element)
+		sort.Strings(tmp)
+		for _, value := range tmp {
+			result = append(result, value)
+		}
+	}
+	return result
 }
